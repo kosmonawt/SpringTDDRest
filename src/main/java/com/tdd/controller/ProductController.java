6b5960120caff1b373313b66eb58455359774448
Kostiantyn Voitsekhov <kosvkv@gmail.com>
2020-08-23 12:18:58 +0200

    Test product found by ID - GET

--- src/main/java/com/tdd/controller/ProductController.java
+++ src/main/java/com/tdd/controller/ProductController.java
@@ -1,8 +1,28 @@
 package com.tdd.controller;
 
 
+import com.tdd.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/product")
+
 public class ProductController {
+
+    final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(productService.findById(id));
+    }
+
 }
--- src/test/java/com/tdd/controller/ProductControllerTest.java
+++ src/test/java/com/tdd/controller/ProductControllerTest.java
@@ -1,13 +1,22 @@
 package com.tdd.controller;
 
+import com.tdd.model.Product;
 import com.tdd.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith({SpringExtension.class})
 @SpringBootTest
@@ -21,5 +30,19 @@ class ProductControllerTest {
     MockMvc mockMvc;
 
 
+    @Test
+    @DisplayName("Test product found - GET /product/1 ")
+    void testGETProductFound() throws Exception {
+
+        Product mockProduct = new Product(1, "Nazwa", "Opis", 1, 1);
+
+        doReturn(mockProduct).when(productService).findById(mockProduct.getId());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/product/{id}", mockProduct.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)));
+
+    }
 
 }
